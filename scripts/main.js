3f8a6fa23df193a90eb383ba3b37b739068db3ee
Jacob <jacob.d.wencloff@gmail.com>
2022-04-25 12:18:56 -0700

    created basic landing Nav buttons and each page

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+//Each landing page nav iteam should have a button associated with it 
+//On click of Landing nav iteam, toggle .hide class on current page and associated page to the button
+$(document).ready(function(){
+    $('#aboutPg, #skillsPg, #projectPg, #contactPg').toggleClass('hide')
+
+    $('li').on('click', function(event){
+        let target = event.currentTarget.id
+        console.log(target)
+        $('#landingContainer').toggleClass('hide')
+
+        switch(target){
+            case 'abt':
+                $('#aboutPg').toggleClass('hide')
+                break;
+            case 'skl':             
+                $('#skillsPg').toggleClass('hide')
+                break;
+            case 'prj':
+                $('#projectPg').toggleClass('hide')
+                break;
+            case 'cnt':
+                $('#contactPg').toggleClass('hide')
+                break;
+        }
+    })
+
+})
