078712cc5888df40ae130a9852421bc1c0878025
Jacob <jacob.d.wencloff@gmail.com>
2022-04-26 14:13:10 -0700

    not sure what i did here but i like it

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,87 @@
 //On click of Landing nav iteam, toggle .hide class on current page and associated page to the button
 $(document).ready(function(){
     $('#aboutPg, #skillsPg, #projectPg, #contactPg, #thedrop').toggleClass('hide')
-    $('#ddNav').slideUp()
-    $('li').css('cursor','pointer').on('click', function(event){
+    $('.ddNav').slideUp()
+
+    let currentPage = null
+
+    $('.landing-but').css('cursor','pointer').on('click', function(event){
         let target = event.currentTarget.id
-        console.log(target)
+    
         $('#landingContainer').toggleClass('hide')
 
         switch(target){
             case 'abt':
                 $('#aboutPg').toggleClass('hide')
+                currentPage = $('#aboutPg')
+                
                 break;
             case 'skl':             
                 $('#skillsPg').toggleClass('hide')
+                currentPage = $('#skillsPg')
+              
                 break;
             case 'prj':
                 $('#projectPg').toggleClass('hide')
+                currentPage = $('#projectPg')
+              
                 break;
             case 'cnt':
                 $('#contactPg').toggleClass('hide')
+                currentPage = $('#contactPg')
+               
                 break;
         }
     })
-  
-    $('.DD-img').css('cursor', "pointer").on('click', function(){
+
+    //We need to create the logic behind the dropdown menu
+    //this includes noticing when the DD items have been picks
+    //and redirecting the user to the proper page after its been clicked
+    $('.DD-img').css('cursor', "pointer").on('click',function(){
         console.log('clicked')
-        $('#ddNav').slideDown()
+        $('.ddNav').slideToggle()
+
+        $('.dd-But').css('cursor', 'pointer').on('click', function(event){
+            let target = event.currentTarget.id
+            console.log(target)
+            switch(target){
+                case 'dd-abt':
+                  
+                    $(currentPage).toggleClass('hide')
+                    $('#aboutPg').toggleClass('hide')
+                    $('.ddNav').slideUp()
+                    currentPage = $('#aboutPg')
+                    break;
+                case 'dd-skl':
+                    
+                    $(currentPage).toggleClass('hide')
+                    $('#skillsPg').toggleClass('hide')
+                    $('.ddNav').slideUp()
+                    currentPage = $('#skillsPg')
+                    break;
+                case 'dd-prj':
+                    
+                    $(currentPage).toggleClass('hide')
+                    $('#projectPg').toggleClass('hide')
+                    $('.ddNav').slideUp()
+                    currentPage = $('#projectPg')
+                    break;
+                case 'dd-cnt':
+                
+                    $(currentPage).toggleClass('hide')
+                    $('#contactPg').toggleClass('hide')
+                    $('.ddNav').slideUp()
+                    currentPage = $('#contactPg')
+                    break;
+                case 'dd-home':
+                   
+                    $(currentPage).toggleClass('hide')
+                    $('#landingContainer').toggleClass('hide')
+                    $('.ddNav').slideUp()
+                    currentPage = $('#landingContainer')
+                    break;
+            }
+        })
     })
+
 })
