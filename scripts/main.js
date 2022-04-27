81e6c89d858994229e09c91b0f8f47d314917261
Jacob <jacob.d.wencloff@gmail.com>
2022-04-27 10:34:55 -0700

    Added quote of the day API along with slideDown animation on the landing Nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,11 @@
 //On click of Landing nav iteam, toggle .hide class on current page and associated page to the button
 $(document).ready(function(){
     $('#aboutPg, #skillsPg, #projectPg, #contactPg, #thedrop').toggleClass('hide')
-    $('.ddNav').slideUp()
+    $('.ddNav' ).slideUp()
+    $('#landing-nav' ).toggleClass('hide').slideUp()
+    window.setTimeout(function(){
+        $('#landing-nav' ).slideDown()
+    }, 500)
 
     let currentPage = null
 
@@ -47,35 +51,30 @@ $(document).ready(function(){
             console.log(target)
             switch(target){
                 case 'dd-abt':
-                  
                     $(currentPage).toggleClass('hide')
                     $('#aboutPg').toggleClass('hide')
                     $('.ddNav').slideUp()
                     currentPage = $('#aboutPg')
                     break;
                 case 'dd-skl':
-                    
                     $(currentPage).toggleClass('hide')
                     $('#skillsPg').toggleClass('hide')
                     $('.ddNav').slideUp()
                     currentPage = $('#skillsPg')
                     break;
-                case 'dd-prj':
-                    
+                case 'dd-prj':                  
                     $(currentPage).toggleClass('hide')
                     $('#projectPg').toggleClass('hide')
                     $('.ddNav').slideUp()
                     currentPage = $('#projectPg')
                     break;
-                case 'dd-cnt':
-                
+                case 'dd-cnt':              
                     $(currentPage).toggleClass('hide')
                     $('#contactPg').toggleClass('hide')
                     $('.ddNav').slideUp()
                     currentPage = $('#contactPg')
                     break;
-                case 'dd-home':
-                   
+                case 'dd-home':                
                     $(currentPage).toggleClass('hide')
                     $('#landingContainer').toggleClass('hide')
                     $('.ddNav').slideUp()
@@ -84,5 +83,19 @@ $(document).ready(function(){
             }
         })
     })
+
+        // API call to zenquotes for desktop landing page
+        // let quoteCall = $.ajax({
+        
+        //     method: "get",
+        //     url: "https://zenquotes.io/api/random/afd050716de4256d0806a52d07ad783c36594faa"
+        // })
+        //     quoteCall.then(function(response){
+        //         console.log(response)
+        //         $('#quote').append(`${response[0].h}`)
+        //     })
+        //     .catch(function(){
+        //         console.log("Error")
+        //     })
 
 })
